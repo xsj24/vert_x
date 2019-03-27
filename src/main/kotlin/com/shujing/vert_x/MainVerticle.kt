fea0d9600d17dec4d0634037099e03593e41340b
xsj <1187080550@qq.com>
2019-03-27 19:32:10 +0800

    vert.x logback

--- src/main/kotlin/com/shujing/vert_x/MainVerticle.kt
+++ src/main/kotlin/com/shujing/vert_x/MainVerticle.kt
@@ -2,8 +2,14 @@ package com.shujing.vert_x
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class MainVerticle : AbstractVerticle() {
+
+  private val log : Logger = LoggerFactory.getLogger(MainVerticle::class.java)
+
+  private val port = 8888
 
   override fun start(startFuture: Future<Void>) {
     vertx
@@ -13,10 +19,10 @@ class MainVerticle : AbstractVerticle() {
           .putHeader("content-type", "text/plain")
           .end("Hello from Vert.x!")
       }
-      .listen(8888) { http ->
+      .listen(port) { http ->
         if (http.succeeded()) {
           startFuture.complete()
-          println("HTTP server started on port 8888")
+          log.info("HTTP server started on port {}", port)
         } else {
           startFuture.fail(http.cause())
         }
